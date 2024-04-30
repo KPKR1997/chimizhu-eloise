6a7f97f53cb547c3a231b7a4d3ae2b9ac531e2d0
KPKR1997 <krishnaprakash997@gmail.com>
2024-05-01 02:31:36 +0530

    second

---
+++ src/pages/readpage.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/readPage.module.css";
+import Link from 'next/link'
+import React from'react'
+import {PageFlip} from 'page-flip';
+import ImageComponent from "@/components/image"
+
+
+export default function Pages() {
+
+const pagenumber = 1;
+
+
+  function arrayConstruct(){
+    let array = [];
+    for (let i=1;i<=100;i++)
+      array.push(i);
+    return(array);
+  }
+  const ImageArray = arrayConstruct();
+  console.log(ImageArray);
+
+
+  const pdf = "./Files/CHIMIZHU_MAGAZINE.pdf";
+  const page1 = "./images/editorial.jpg"
+
+  
+    return (
+      <>
+      <div className = {styles.main}>
+         <object data={pdf} className={styles.object}/>
+      </div>
+      </>
+    )
+  }
